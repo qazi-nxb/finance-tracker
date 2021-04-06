c43ac8756b80851d026e9d29fb442bdd3d981a7c
faheem <qazi.faheem@nxb.com.pk>
2021-04-07 03:26:16 +0500

    added some browser functionalities

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,23 @@
 class StocksController < ApplicationController
     def search
-        stock = Stock.look_up(params[:stock])
-        render json: stock
+        if params[:stock].present?
+            @stock = Stock.look_up(params[:stock])
+            if @stock
+                respond_to do |format|
+                    format.js { render partial: 'users/result'}
+                end
+            else
+                #binding.pry
+                respond_to do |format|
+                    flash.now[:alert] = "please enter a valid symbol to search"
+                    format.js { render partial: 'users/result'}
+                end
+            end
+        else
+            respond_to do |format|
+                flash.now[:alert] = "please enter a symbol to search"
+                format.js { render partial: 'users/result'}
+            end
+        end
     end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,11 +1,16 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
 
     def self.look_up(ticker_symbol)
         client = IEX::Api::Client.new(
                     publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                      endpoint: 'https://sandbox.iexapis.com/v1'
                     )
-        client.price(ticker_symbol)
-        
+        begin
+        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        rescue => exception
+          return nil
+        end
     end
 end
